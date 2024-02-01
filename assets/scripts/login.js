9d94eae05ca7693a7b6a7362b87e3bf7a9c6da51
Pedro Medina <pmedina-2019111@kinal.edu.gt>
2024-01-31 22:02:49 -0600

    Login con logica y css

--- assets/scripts/login.js
+++ assets/scripts/login.js
@@ -3,5 +3,22 @@ document.addEventListener('DOMContentLoaded', function(){
     loginForm.addEventListener('submit', function (event){
         event.preventDefault();
         login();
-    })
-})
+    });
+});
+
+function login(){
+    const emailInput = document.getElementById('email');
+    const passwordInput = document.getElementById('password');
+    const email = emailInput.value.trim();
+    const password = passwordInput.value.trim();
+
+    if (email === 'pmedina-2019111@kinal.edu.gt' && password === '0') {
+        
+        alert('Inicio de sesión exitoso. Redirigiendo a la página de contactos.');
+        window.location.href = 'contacts.html'; 
+      } else {
+        // Autenticación fallida
+        alert('Inicio de sesión fallido. Verifica tu correo y contraseña.');
+      }
+}
+
